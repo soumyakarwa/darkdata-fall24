cf2339a83b22794371536930bbb9ae1684dce699
soumyakarwa <soumyakarwa01@gmail.com>
2024-12-09 09:53:39 -0500

    added dummy text

--- src/routes/directions/[category]/[id]/+page.svelte
+++ src/routes/directions/[category]/[id]/+page.svelte
@@ -2,19 +2,23 @@
     export let articleData;
 </script>
 
-<section>
+<section class="article-content">
     <div>
         {#if articleData.content.images && articleData.content.images.length > 0}
             {#each articleData.content.images as image}
                 <img class="article-img" src={image.url} alt={image.description || 'Article Image'} />
             {/each}
         {/if}
-        <p>{articleData.content.body}</p>
+        <p>{@html articleData.content.body}</p>
     </div>
 </section>
 
 <style>
     .article-img{
       width: 100%; 
+    }
+
+    .article-content{
+        font-family: var(--font-body); 
     }
 </style>
